cd1d1c42db826f500d0dba4f32e1cfe70e76ce39
Fedor Nikolaev <fsqcds@gmail.com>
2015-01-25 19:44:02 -0500

    Fix order of columns

--- plots/make-plot.R
+++ plots/make-plot.R
@@ -8,7 +8,7 @@ extract.attributes <- function(jsonString) {
     fromJSON(paste('{',jsonString,'}'))
 }
 
-for (file in c("unigrams", "od", "unw")) {
+for (file in c("unigrams", "od", "unw")) {#, "fsdm")) {
     csv <- read.csv(paste(file, ".csv", sep=""), header = FALSE, col.names=c("filename", "weights.json"),
                     stringsAsFactors = FALSE)[c(1:5,7:11,13:17,19:23),]
 
@@ -16,13 +16,17 @@ for (file in c("unigrams", "od", "unw")) {
     row.names(data) <- NULL
     data <- data.frame(t(apply(data, 1, function(x){x/sum(x)})))
     data$collection <- sapply(csv$filename, extract.collection.name)
-    data <- data[c(6,1:5)]
-    data <- data.frame(do.call(rbind, by(data[, 2:6], data[, 1], colMeans)))
+    ncol <- dim(data)[2]
+    data <- data[c(ncol,1:ncol-1)]
+    data <- data.frame(do.call(rbind, by(data[, 2:ncol], data[, 1], colMeans)))
     names(data) <- sapply(names(data), function(n){tail(strsplit(n, "\\.")[[1]],n=1)})
-    data$group <- as.factor(row.names(data))
-    levels(data$group) <- c("SemSearch_ES", "ListSearch", "INEX_LD", "QALD2")
+    data$group <- factor(row.names(data), levels=c("SemSearch_ES", "ListSearch", "INEX_LD", "QALD2"))
     cairo_ps(paste(file, ".eps", sep=""))
-    formula <- attributes + categories + names + outgoingentitynames + similarentitynames ~ group
+    if (file == "fsdm") {
+        formula <- a ~ b
+    } else {
+        formula <- attributes + categories + names + outgoingentitynames + similarentitynames ~ group
+    }
     print(stripplot(formula, data=data, ylab = "average field weights",
               par.settings = list(superpose.symbol = list(pch = 1:5)), auto.key = list(space = "right",
                                                                            border=TRUE, padding.text=4)))
